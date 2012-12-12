adda83e44eac4e61292007ae09422bad94a1eb4d
Mirko Kiefer <mail@mirkokiefer.com>
2012-12-12 17:05:46 +0100

    extend test to have n-way merge

--- test.js
+++ test.js
@@ -23,14 +23,20 @@ var modified2 = [
   {id: 1, value: 9},
   {id: 4, value: 5}
 ]
+var modified3 = [
+  {id: 1, value: 10},
+  {id: 2, value: 2},
+  {id: 4, value: 4},
+]
 
 var diff1 = diff(origin, modified1)
 var diff2 = diff(origin, modified2)
+var diff3 = diff(origin, modified3)
 
 var mergeExpected = {
   valueConflicts: [1, 3],
   values: {
-    1: [5, 9],
+    1: [5, 9, 10],
     2: 2,
     3: [8, null],
     4: 5,
@@ -39,13 +45,15 @@ var mergeExpected = {
   orderConflicts: true,
   order: [
     [6, 3, 2, 4, 1],
-    [6, 3, 1, 2, 4]
+    [6, 3, 1, 2, 4],
+    [1, 6, 3, 2, 4]
   ]
 }
 
 describe('id-merge', function() {
   it('should merge based on the diffs', function() {
-    var merged = merge([diff1, diff2])
+    var merged = merge([diff1, diff2, diff3])
+    console.log(merged)
     assert.deepEqual(merged, mergeExpected)
   })
 })
